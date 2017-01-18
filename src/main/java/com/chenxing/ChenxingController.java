afb2478271d54319c3efd9a80f054d6fc1d4b39d
sheepstarli <licx@easemob.com>
2017-01-18 17:39:35 +0800

    fix main problem

--- src/main/java/com/chenxing/ChenxingController.java
+++ src/main/java/com/chenxing/ChenxingController.java
@@ -1,9 +1,7 @@
 package com.chenxing;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/chenxing/ZendeskController.java
+++ src/main/java/com/chenxing/ZendeskController.java
@@ -1,11 +1,7 @@
 package com.chenxing;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.Verification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,19 +52,4 @@ public class ZendeskController {
         return result;
     }
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String token = JWT.create()
-                .withIssuedAt(new Date())
-                .withJWTId(UUID.randomUUID().toString())
-                .withClaim("email", "licx@easemob.com")
-                .withClaim("name", "小星星")
-                .sign(Algorithm.HMAC256(JWT_SECRET));
-        System.out.println(token);
-        JWT decode = JWT.decode(token + "1");
-        System.out.println(decode.getClaim("email").asString());
-        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(JWT_SECRET)).build();
-        DecodedJWT verify = verifier.verify(token + "1");
-        System.out.println(verify.getClaim("email").asString());
-
-    }
 }
